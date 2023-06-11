a22dd16c9db5d42c81d532eb6102bc1e03ac5153
neox56 <neox56@gmail.com>
2023-06-11 13:33:00 +0400

    add PHP_CodeSniffer and validate code use PSR12

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * Application entry point
+ *
+ * PHP version 8.1
+ *
+ * @category PHP
+ * @package  Not_Package
+ * @author   Dmitry Smolin <neox56@gmail.com>
+ * @license  https://github.com/DmitriySmolin/hexlet-php/master/licence.txt BSD
+ * @link     not link
+ */
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Файл не включается напрямую
--- src/Runner.php
+++ src/Runner.php
@@ -1,12 +1,31 @@
 <?php
 
+/**
+ * Convert collection to upper case
+ *
+ * PHP version 8.1
+ *
+ * @category PHP
+ * @package  PHP_Runner
+ * @author   Dmitry Smolin <neox56@gmail.com>
+ * @license  https://github.com/DmitriySmolin/hexlet-php/master/licence.txt BSD
+ * @link     http://pear.php.net/package/PHP_Runner
+ */
+
 namespace Hexlet\Php\Runner;
 
+/**
+ * Function that converts to uppercase
+ *
+@return array
+ */
 function run()
 {
-    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
-        return strtoupper($name);
-    });
+    $collection = collect(['taylor', 'abigail', null])->map(
+        function ($name) {
+            return strtoupper($name);
+        }
+    );
 
     return $collection;
 }
